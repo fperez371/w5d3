c6eb319d78d0ab518d6fe8cc7e2f1b5164105920
Francisco <francisco.e.perez@gmail.com>
2019-01-02 13:54:00 -0500

    finish phase 1

--- rails_lite/bin/p01_basic_server.rb
+++ rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,19 @@
 require 'rack'
+require 'byebug'
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    if req.path
+        res.write("#{req.path}")
+    else
+    res.write('hello world!')
+    end
+    res.finish
+end
+
+Rack::Server.start(
+    app: app,
+    Port: 3000
+)
