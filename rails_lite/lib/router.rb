98c27dfb24bca5ffb639b8a82a94c0c1f54acb1b
Francisco <francisco.e.perez@gmail.com>
2019-01-02 17:29:46 -0500

    finish half of phase 5

--- rails_lite/lib/router.rb
+++ rails_lite/lib/router.rb
@@ -16,8 +16,7 @@ class Route
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
-  def run(req, res)
-    
+  def run(req, res) 
   end
 end
 
@@ -25,10 +24,17 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(
+      pattern,
+      method,
+      controller_class,
+      action_name
+    )
   end
 
   # evaluate the proc in the context of the instance
@@ -39,13 +45,18 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    raise 404
   end
 end
